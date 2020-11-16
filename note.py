69d1bea71c976057676bfa9ec0892d715cfa74a8
SzymeczeqTechManieczeq <68206764+SzymeczeqTechManieczeq@users.noreply.github.com>
2020-11-16 08:15:23 +0100

    1.00v
    1.00v

--- note.py
+++ note.py
@@ -1,13 +1,7 @@
-print("every note is writed")
+ = "text"
 while True:
-	a = input("write: ")
-	print(a)
-	f= open("autosave.txt","w+")
-	f.write(a)
-	f.close() 
-	b = input("do u want to save it? y/n ")
-	if b == 'y':
-		print('this file will name save.txt and it is only 1 file')
-		f= open("save.txt","w+")
-		f.write(b)
-		f.close() 
+	a = input()
+	b = b + "\n" + a
+	f= open("note.txt","w+")
+	f.write(b)
+	f.close()
