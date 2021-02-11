ac3206cf5ed427526beb982556ea24fbdc0eb56d
SzymeczeqTechManieczeq <68206764+SzymeczeqTechManieczeq@users.noreply.github.com>
2021-02-11 09:44:55 +0100

    I added a things
    and that is great

---
+++ div.sh
@@ -0,0 +1,2 @@
+python3 note.py
+python note.py
--- note.py
+++ note.py
@@ -1,9 +1,13 @@
 try:
+    print("Write name and localisation of file which you want to edit or which you want to do.")
     b = "text"
+    d = input()
+    text = open(d,"a+").read()
+    print(text)
     while True:
         a = input()
         b = b + "\n" + a
-        f= open("note.txt","w+")
+        f= open(d,"w+")
         f.write(b)
         f.close()
 except KeyboardInterrupt:
